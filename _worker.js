32f38152869804546f4ac2c97c94f4f70fb06304
duanso <61611765+duanso@users.noreply.github.com>
2025-03-01 17:06:48 +0800

    Update _worker.js，修改名称

--- _worker.js
+++ _worker.js
@@ -23,8 +23,8 @@ https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
 `
 
 let urls = [];
-let subConverter = "psub.yfeiduan.workers.dev"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/duanso/Custom_OpenClash_Rules/refs/heads/main/Custom_Clash.ini"; //订阅配置文件
+let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
